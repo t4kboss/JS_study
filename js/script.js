ff97db9d6e6cd7b7c82c0ca25d4ac2e805f5bde4
Alexander Fedorov <t4kboss@gmail.com>
2021-11-11 18:34:02 +0200

    task 018

--- js/script.js
+++ js/script.js
@@ -1,21 +1,64 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false,
+  privat: true,
 };
 
-let a = prompt("Один из последних просмотренных фильмов?", "");
-let b = prompt("На сколько оцените его?", "");
-let c = prompt("Один из последних просмотренных фильмов?", "");
-let d = prompt("На сколько оцените его?", "");
+function rememberMyFilms() {
+  for (let index = 0; index < 2; index++) {
+    let a = prompt("Один из последних просмотренных фильмов?", "");
+    let b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length <= 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      index--;
+    }
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    let answer = prompt(`Ваш любимый жанр под номером ${i + 1}`, "");
+    personalMovieDB.genres[i] = answer;
+  }
+}
+
+writeYourGenres();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+  } else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+  } else {
+    alert("Произошла ошибка");
+  }
+}
+detectPersonalLevel();
+
+function showMyDB() {
+  if (personalMovieDB.privat) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDB();
