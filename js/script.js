18e4e1c7c41b5854d886e4fd3da88bc03b12dfee
Alexander Fedorov <t4kboss@gmail.com>
2021-11-11 15:51:58 +0200

    one more time

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let a = prompt("Один из последних просмотренных фильмов?", "");
+let b = prompt("На сколько оцените его?", "");
+let c = prompt("Один из последних просмотренных фильмов?", "");
+let d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
